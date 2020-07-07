2528020fdb1810bd14eefa7d980ea74042bf99a5
o2sevruk <o2sevruk@gmail.com>
2020-07-07 13:33:53 +0300

    type: new functionality description: Context added.

--- src/App.js
+++ src/App.js
@@ -1,41 +1,47 @@
 import React from 'react';
-import { render } from 'react-dom';
+import {render} from 'react-dom';
 
-function Parent ({ name }) {
-  return (
-    <div>
-      <h1>Parent</h1>
-      <Child name={name}/>
-    </div>
-  );
+const Context = React.createContext();
+
+function Parent({name}) {
+	return (
+		<div>
+			<h1>Parent</h1>
+			<Child name={name} />
+		</div>
+	);
 }
 
-function Child ({ name }) {
-  return (
-    <div>
-      <h1>Child</h1>
-      <Grandchild name={name}/>
-    </div>
-  );
+function Child({name}) {
+	return (
+		<div>
+			<h1>Child</h1>
+			<Grandchild name={name} />
+		</div>
+	);
 }
 
-function Grandchild ({ name }) {
-  return (
-    <div>
-      <h1>Grandchild</h1>
-      <h3>Name: {name}</h3>
-    </div>
-  );
+function Grandchild({name}) {
+	return (
+		<Context.Consumer>
+			{(name) => (
+				<div>
+					<h1>Grandchild</h1>
+					<h3>Name: {name}</h3>
+				</div>
+			)}
+		</Context.Consumer>
+	);
 }
 
 class App extends React.Component {
-  render() {
-    const name = 'Tyler';
-    
-    return (
-      <Parent />
-    );
-  }
+	render() {
+		const name = 'Tyler';
+		
+		return (
+			<Context.Provider value={name}> <Parent /> </Context.Provider>
+		);
+	}
 }
 
 export default App;
